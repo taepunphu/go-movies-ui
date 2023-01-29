a521ee4f0be1206bbd6a7c6b60439e9cfcf5b932
kiattisak phanphu <kiattisak.pa@outlook.com>
2023-01-29 20:56:26 +0700

    first commit

---
+++ src/components/EditMovie.js
@@ -0,0 +1,11 @@
+const EditMovie = () => {
+    return (
+      <div className="text-center">
+        <h2>Edit Movie</h2>
+        <hr />
+      </div>
+    );
+  };
+  
+  export default EditMovie;
+  
---
+++ src/components/Genres.js
@@ -0,0 +1,11 @@
+const Genres = () => {
+    return (
+      <div className="text-center">
+        <h2>Genres</h2>
+        <hr />
+      </div>
+    );
+  };
+  
+  export default Genres;
+  
